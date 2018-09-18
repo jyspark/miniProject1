e5f7d9bd4a8bc482f778ff939f06f2e08f5acffc
jyspark <pys4330@gmail.com>
2018-09-18 19:51:55 -0400

    Corrected Errors and added statements

--- getandread.py
+++ getandread.py
@@ -20,48 +20,46 @@ def gettweets(userID):
     auth = tweepy.OAuthHandler(tweetkey.consumer_key, tweetkey.consumer_secret)
     auth.set_access_token(tweetkey.access_key, tweetkey.access_secret)
     api = tweepy.API(auth)
-    
-
-    try:         #Run when the handle is existed 
+           
+    try:
+           #Run when the handle is existed 
         user= api.get_user(userID)
+        #initialize a list to hold all the tweepy Tweets
+    except:
+        print("Not Correct Handle or Does not Exist.")
+        gettweets(input("Enter Again: @"))    
 
-        ##initialize a list to hold all the tweepy Tweets
-        initTweet = []    
+    initTweet = []    
     
-        ##make initial request for most recent tweets (200 is the maximum allowed count)
-        newtweets = api.user_timeline(screen_name = userID,count=200)
+    ##make initial request for most recent tweets (200 is the maximum allowed count)
+    newtweets = api.user_timeline(screen_name = userID,count=20)
 
+    ##save most recent tweets
+    initTweet.extend(newtweets)
+    
+    ##save the id of the oldest tweet less one
+    oldest = initTweet[-1].id - 1
+    
+    ##keep grabbing tweets until there are no tweets left to grab
+    while len(newtweets) > 0:
+      
+            ##all subsiquent requests use the max_id param to prevent duplicates
+        newtweets = api.user_timeline(screen_name = userID,count=20,max_id=oldest)
+        
         ##save most recent tweets
         initTweet.extend(newtweets)
-    
-        ##save the id of the oldest tweet less one
+       
+        ##update the id of the oldest tweet less one
         oldest = initTweet[-1].id - 1
-    
-        ##keep grabbing tweets until there are no tweets left to grab
-        while len(newtweets) > 0:
-        
-            ##all subsiquent requests use the max_id param to prevent duplicates
-            newtweets = api.user_timeline(screen_name = userID,count=200,max_id=oldest)
-        
-            ##save most recent tweets
-            initTweet.extend(newtweets)
-        
-            ##update the id of the oldest tweet less one
-            oldest = initTweet[-1].id - 1
-            if(len(initTweet) > 15):
-                break
+        if(len(initTweet) > 2000):
+            break
               
         #print(initTweet)
-        print("Got tweets.\n")
-
+    print("Got tweets.\n")
         ##Call function getmediaURLS to get medial_URLs
-        getmediaURLs(initTweet)
+    getmediaURLs(initTweet)
 
-    except Exception:
-        tweethandle = input("Not correct Handle. \nEnter again: @")
-        gettweets("@"+tweethandle)
-    
-
+  
 ##Check the availability of Twitter  
 socket.setdefaulttimeout(20)
 url = 'http://twitter.com/'
@@ -75,7 +73,9 @@ else:
     if __name__ == '__main__':
         #pass in the username of the account you want to download
         tweethandle = input("Enter the Username: @")
-        gettweets("@"+tweethandle)
+        gettweets(tweethandle)
+
+
 
 
     
