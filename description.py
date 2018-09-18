c2fae8e3990578b25548d7a96ad40d5fe6a0c45c
jyspark <pys4330@gmail.com>
2018-09-18 19:51:05 -0400

    Run after FFmpeg

--- description.py
+++ description.py
@@ -2,22 +2,21 @@ from google.cloud import videointelligence
 import io
 import sys
 
-
 video_client = videointelligence.VideoIntelligenceServiceClient()
 features = [videointelligence.enums.Feature.LABEL_DETECTION]
 
-with io.open('/media/sf_Assginment1/EC601/miniProject1/video.mp4', 'rb') as video:
+with io.open('/media/sf_Assginment1/EC601/miniProject1/movie.mp4', 'rb') as video:
 	print("Opening Video")
 	input_content = video.read()
-try: 
-	operation = video_client.annotate_video(features=features, input_content=input_content)
-	print('\nProcessing Video for label Annotations')
-	result = operation.result(timeout=90)
-	print('\nFinished processing.')
-except Exception as e:
-	print("Error")
-	print(e)
-	exit()
+	try: 
+		operation = video_client.annotate_video(features=features, input_content=input_content)
+		print('\nProcessing Video for label Annotations')
+		result = operation.result(timeout=90)
+		print('\nFinished processing.')
+	except Exception as e:
+		print("Error")
+		print(e)
+		exit()
 
 segment_labels = result.annotation_results[0].segment_label_annotations
 
