d690c3eacaa9eff1ec26aeb185a4f9f41cf7484c
jyspark <pys4330@gmail.com>
2018-09-18 16:35:10 -0400

    checking existence of User

--- getandread.py
+++ getandread.py
@@ -9,11 +9,8 @@ import tweepy #https://github.com/tweepy/tweepy
 #access_key = "Enter Access Token"
 #access_secret = "Enter Access Token secret"
 
-
-
+#Twitter only allows access to a users most recent 3240 tweets with this method
 def gettweets(userID):
-
-    #Twitter only allows access to a users most recent 3240 tweets with this method
 
     from mediaURLs import getmediaURLs   
    
@@ -22,49 +19,47 @@ def gettweets(userID):
     auth.set_access_token(tweetkey.access_key, tweetkey.access_secret)
     api = tweepy.API(auth)
     
-    ##initialize a list to hold all the tweepy Tweets
-    initTweet = []    
+    try: 
+        user= api.get_user(userID)
+
+                ##initialize a list to hold all the tweepy Tweets
+        initTweet = []    
     
-    ##make initial request for most recent tweets (200 is the maximum allowed count)
-    newtweets = api.user_timeline(screen_name = userID,count=200)
-    
-    ##save most recent tweets
-    initTweet.extend(newtweets)
-    
-    ##save the id of the oldest tweet less one
-    oldest = initTweet[-1].id - 1
-    
-    ##keep grabbing tweets until there are no tweets left to grab
-    while len(newtweets) > 0:
-        
-        ##all subsiquent requests use the max_id param to prevent duplicates
-        newtweets = api.user_timeline(screen_name = userID,count=200,max_id=oldest)
-        
+        ##make initial request for most recent tweets (200 is the maximum allowed count)
+        newtweets = api.user_timeline(screen_name = userID,count=200)
+
         ##save most recent tweets
         initTweet.extend(newtweets)
-        
-        ##update the id of the oldest tweet less one
+    
+        ##save the id of the oldest tweet less one
         oldest = initTweet[-1].id - 1
-        if(len(initTweet) > 15):
-            break
     
-    ##This can be uncommented to work with JSON file   
-    #write tweet objects to JSON
-    #file = open('tweet.json', 'w') 
-    #print ("Writing tweet objects to JSON please wait...")
-    #for status in alltweets:
-    #    json.dump(status._json,file,sort_keys = True,indent = 4)
-    #close the file
-    #print ("Done")
-    #file.close()
-    
-    #print(initTweet)
+        ##keep grabbing tweets until there are no tweets left to grab
+        while len(newtweets) > 0:
+        
+            ##all subsiquent requests use the max_id param to prevent duplicates
+            newtweets = api.user_timeline(screen_name = userID,count=200,max_id=oldest)
+        
+            ##save most recent tweets
+            initTweet.extend(newtweets)
+        
+            ##update the id of the oldest tweet less one
+            oldest = initTweet[-1].id - 1
+            if(len(initTweet) > 15):
+                break
+              
+        #print(initTweet)
+        print("Got tweets.\n")
 
-    ##Call function getmediaURLS to get medial_URLs
-    getmediaURLs(initTweet)
+        ##Call function getmediaURLS to get medial_URLs
+        getmediaURLs(initTweet)
 
+    except Exception:
+        tweethandle = input("Not correct Handle. \nEnter again: @")
+        gettweets("@"+tweethandle)
     
     
 if __name__ == '__main__':
     #pass in the username of the account you want to download
-    gettweets("@BUAthletics")
+    tweethandle = input("Enter the Username: @")
+    gettweets("@"+tweethandle)
