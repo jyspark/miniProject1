106654fd8bb95d0f3427c3740c8d99052b3de9eb
jyspark <pys4330@gmail.com>
2018-09-18 19:50:01 -0400

    Corrected Errors

--- FFmpeg.py
+++ FFmpeg.py
@@ -1,3 +1,13 @@
 import os 
 
-os.popen('ffmpeg -start_number 0 -framerate 1/2 -i /media/sf_Assginment1/EC601/miniProject1/Images/image%d.jpg -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -r 30 -pix_fmt yuv420p video.mp4')
+
+file = './video.mp4'
+if os.path.exists(file):
+	os.remove('movie.mp4')
+	
+os.popen('ffmpeg -start_number 0 -framerate 1/2 -i /media/sf_Assginment1/EC601/miniProject1/Images/image%d.jpg -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -c:v libx264 -r 30 -pix_fmt yuv420p movie.mp4')
+print('\n Analysing the video')
+
+os.system('python description.py')
+
+	
