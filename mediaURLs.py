90c5cb9047291db64b38cd05e9923f5ab8843937
jyspark <pys4330@gmail.com>
2018-09-22 15:33:33 -0400

    added exception

--- mediaURLs.py
+++ mediaURLs.py
@@ -1,7 +1,9 @@
+##2018 Julie Park
 import sys
 
 def getmediaURLs(initTweet): 
 	from getimage import getimage   
+	from getandread import gettweets
     
     ##initialize a list to hold all mediaURLs
 	initMedia = []
@@ -12,12 +14,20 @@ def getmediaURLs(initTweet):
 		if len(media) != 0:
 			url_media = media[0]['media_url']
 			file_type = url_media.split('.')[-1]
-			if file_type == 'jpg':
+			##Change to other extension if needed
+			if file_type == 'jpg':		
 				initMedia.append(media[0]['media_url'])
+
+	##No images to download
+	if len(initMedia) == 0:
+		print("NO Images found.\n")
+		gettweets(input("Enter Username: @"))
 		
 	#print(initMedia) 
-	 
+	
+	##pass the list of image URLs 
 	getimage(initMedia)
+	
 
     
   
